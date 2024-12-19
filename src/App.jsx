14d328c690253e2ff21734c4f5812777bc114a6f
shourya-r <rajputshourya7@gmail.com>
2024-12-19 19:51:06 +0530

    Added local storage support and fixed toggleComplete function destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoProvider } from "./contexts";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -22,11 +24,25 @@ function App() {
     setTodos((prev) =>
       prev.map((prevTodo) =>
         prevTodo.id === id
-          ? { completed: !prevTodo.completed, ...prevTodo }
+          ? { ...prevTodo, completed: !prevTodo.completed }
           : prevTodo
       )
     );
   };
+
+  // retrieve value from local storage initially
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  // add item to local storage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <TodoProvider
@@ -37,9 +53,15 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
           <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
